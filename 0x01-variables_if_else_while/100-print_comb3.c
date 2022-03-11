6c7a2cbcdf24264f89e71651507785f473df5ea5
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-03-11 05:44:28 -0800

    10. invention = brains + material

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - function will print all possible different combinations of two digits
+ *      - but the two digits must be different
+ *      - and permutations are not allowed e.g print only 12 and not 21
+ *      - in the case of permutations, print the smaller number
+ * Return: 0 for success
+ */
+int main(void)
+{
+int i, j;
+
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8)
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
