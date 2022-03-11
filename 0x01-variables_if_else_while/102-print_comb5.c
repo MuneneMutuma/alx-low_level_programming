ba576456641bd8cc65821ebbc1d9448380bacaa6
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-03-11 06:38:59 -0800

    102.1 corrected the limiting conditions for for loops

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,9 +17,9 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-for (k = 0; k >= i; k++)
+for (k = i; k < 10; k++)
 {
-for (l = 0; l > j; l++)
+for (l = j + 1; l < 10; l++)
 {
 putchar(i + '0');
 putchar(j + '0');
