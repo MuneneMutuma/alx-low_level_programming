e5b99673b44ec0c33026534f34f4cac45d7b5e49
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-03-11 06:23:44 -0800

    12. talk about software eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - function will print all possible different combinations of two digits
+ *      - but the two digits must be different
+ *      - and permutations are not allowed e.g print only 12 and not 21
+ *      - in the case of permutations, print the smaller number
+ * Return: 0 for success
+ */
+int main(void)
+{
+int i, j, k, l;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (k = 0; k >= i; k++)
+{
+for (l = 0; l > j; l++)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(k + '0');
+putchar(l + '0');
+if (i != 9 || j != 8)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
