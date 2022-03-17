3566afba955af541037c1f4d4a799c8ead22c4cc
Munene <muneneemasonmutuma@gmail.com>
2022-03-17 17:34:02 +0300

    0. quite the _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point for the program
+ *
+ * Return: 0 if successful
+ *
+ *
+ **/
+int main(void)
+{
+char string[9] = "_putchar";
+int i, length;
+length = sizeof(string)/sizeof(char);
+for (i = 0; i < length; i++)
+_putchar(string[i]);
+_putchar('\n');
+return (0);
+}
