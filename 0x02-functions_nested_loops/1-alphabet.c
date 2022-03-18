ec4fe262853109eec22ab6718cb861c3e17cc128
Munene <muneneemasonmutuma@gmail.com>
2022-03-18 08:47:19 +0300

    1.2 reduced iteration variable to 26

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,7 +12,7 @@ void print_alphabet(void)
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 27; i++)
+	for (i = 0; i < 26; i++)
 		_putchar(alphabet[i]);
 	_putchar('\n');
 }
