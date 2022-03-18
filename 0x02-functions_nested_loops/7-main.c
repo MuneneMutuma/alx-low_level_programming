6038ebc03bdf8a6c01a49fff061d04ce22798109
Munene <muneneemasonmutuma@gmail.com>
2022-03-18 16:34:12 +0300

    7.1 absolute of remainder, not remainder of absolute

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * main - check the code
@@ -10,6 +11,8 @@ int main(void)
 	int r;
 
 	print_last_digit(-98);
+	print_last_digit(INT_MIN);
+	print_last_digit(INT_MAX);
 	print_last_digit(0);
 	r = print_last_digit(-1024);
 	_putchar('0' + r);
