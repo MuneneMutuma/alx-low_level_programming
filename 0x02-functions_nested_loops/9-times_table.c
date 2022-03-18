da13a7c2c495a582b11f2cec2c7eab86cacdf6e6
Munene <muneneemasonmutuma@gmail.com>
2022-03-18 17:33:50 +0300

    9. times-table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints out the 9 times table starting with 0
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int product = i * j;
+
+			if (j == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+			}
+			else
+			{
+				_putchar(' ');
+				if (product < 10)
+					_putchar(' ');
+				else
+					_putchar((product / 10) + '0');
+				
+				_putchar((product % 10) + '0');
+				if (j != 9)
+					_putchar(',');
+			}
+		}
+		_putchar('\n');
+
+	}
+}
