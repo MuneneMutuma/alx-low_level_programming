a3ace9fc63bfbbfebfa8142a9ab21200a5406497
Munene <muneneemasonmutuma@gmail.com>
2022-03-20 12:24:24 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers
+ *
+ * @a: integer to be added to @b
+ * @b: integer to be added to @a
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
