2634a83851b451d093def0cbcc1d4deebd11235f
Munene <muneneemasonmutuma@gmail.com>
2022-03-20 23:28:38 +0300

    100. max 15 mulitiplication table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table of n if n is between 0 and 15
+ * @n: number of the times table we are printing
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				int product = i * j;
+				if (j == 0)
+				{
+					_putchar(j + '0');
+				}
+
+				if ((product / 100) > 0)
+				{
+					int hundreds = product / 100;
+					int tens = ((product % 100) - (product % 10)) / 10;
+					int ones = (product % 10);
+					_putchar(hundreds + '0');
+					_putchar(tens + '0');
+					_putchar(ones + '0');
+				}
+
+				else if ((product / 10) > 0 && (product / 100) == 0)
+				{
+					int tens = product / 10;
+					int ones = product % 10;
+					_putchar(' ');
+					_putchar(tens + '0');
+					_putchar(ones + '0');
+				}
+				else if ((product >= 0) && j != 0 && product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+		
+	}
+}
