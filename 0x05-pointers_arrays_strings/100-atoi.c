492c13a5f6bed0a707d9ea573053ca03b534e733
Munene <muneneemasonmutuma@gmail.com>
2022-03-21 17:38:18 +0300

    100. atoi successfull!

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,45 @@
+/**
+ * _atoi - converts string to integer
+ *
+ * @s: the string to be converted into integer
+ *
+ * Return: int; integer value of the conversion
+ */
+int _atoi(char *s)
+{
+	int i, strlen, negative, number;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	strlen = i;
+	i = 0;
+	negative = 0;
+	number = 0;
+
+	while (i < strlen)
+	{
+		if (s[i] == '-')
+		{
+			negative++;
+
+		}
+		while (s[i] >= '0' && s[i] <= '9' && s[i] != '\0')
+		{
+			number *= 10;
+			number += (s[i] - '0');
+			i++;
+		}
+		if (s[i - 1] >= '0' && s[i - 1] <= '9')
+			i = strlen;
+		i++;
+	}
+
+	if (negative % 2)
+		return (-number);
+	else
+		return (number);
+}
