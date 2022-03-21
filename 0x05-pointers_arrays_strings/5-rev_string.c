32012197a7314a9d5f84635e727e4464cc4f3b68
Munene <muneneemasonmutuma@gmail.com>
2022-03-21 17:07:57 +0300

    5. reverted finally; was quite a hustle

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,47 @@
+int _strlen(char *c);
+void _strcpy(char *dest, char *src);
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, strlen;
+	char tmp;
+
+	strlen = _strlen(s);
+	i = 0;
+
+	while (i < strlen/2)
+	{
+		tmp = s[i];
+		s[i] = s[strlen - 1 - i];
+		s[strlen - 1 - i] = tmp;
+
+		i++;
+	}
+}
+
+/**
+ * _strlen - finds the length of a string
+ *
+ * @c: string whose length is evaluated
+ *
+ * Return: int; length of the string @c
+ */
+int _strlen(char *c)
+{
+	int i;
+	
+	i = 0;
+	while (c[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
