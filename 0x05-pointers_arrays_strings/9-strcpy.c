2cf62804d08cfd5ac061070afffb05ce88e7a150
Munene <muneneemasonmutuma@gmail.com>
2022-03-21 16:07:59 +0300

    9. _strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+/**
+ * _strcpy - copies the string @src to @dest
+ *
+ * @src: source string
+ * @dest: destination
+ *
+ * Return: char * - a char pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[(i + 1)] = '\0';
+
+	return (dest);
+}
