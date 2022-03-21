921a7c17342e0f7352880cfd9683470ba2c9d028
Munene <muneneemasonmutuma@gmail.com>
2022-03-21 12:21:17 +0300

    4. print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: the string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int i, strlen;
+
+	i = 0;
+	strlen = 0;
+	while (s[i] != '\0')
+	{
+		strlen++;
+		i++;
+	}
+
+	while (strlen >= 0)
+	{
+		_putchar(s[strlen]);
+		strlen--;
+	}
+
+	_putchar('\n');
+}
