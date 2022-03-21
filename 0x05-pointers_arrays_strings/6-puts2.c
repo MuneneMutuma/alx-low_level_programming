fd66bd7f77bc670022c6fe607cd49d2b484fca6c
Munene <muneneemasonmutuma@gmail.com>
2022-03-21 15:23:47 +0300

    6.1 check both i and 2i

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,10 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[2 * i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		_putchar(str[2 * i]);
+		i += 1;
 	}
 
 	_putchar('\n');
