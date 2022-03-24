aad9a23b44934de599b7397ac96039f7ca82085a
Munene <muneneemasonmutuma@gmail.com>
2022-03-24 12:42:57 +0300

    6. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+/**
+ * cap_string - camelcases words separated by
+ *		, ; . ! ? " ( ) { }
+ *
+ * @str: the string to be camelcased
+ *
+ * Return: pointer to the camelcased string
+ */
+char *cap_string(char *str)
+{
+	int i, sep;
+	char separator[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (sep = 0; sep < 13; sep++)
+		{
+			if (str[i] == separator[sep] && str[i + i] <= 122 && str[i + 1] >= 97)
+				str[i + 1] = (str[i + 1] - 32) + '\0';
+		}
+	}
+	return (str);
+}
