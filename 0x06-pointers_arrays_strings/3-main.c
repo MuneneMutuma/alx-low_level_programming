bc4eafed01311c17ea2adcfd2438d142801e4410
Munene <muneneemasonmutuma@gmail.com>
2022-03-24 11:56:24 +0300

    3.2 betty corrections

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -14,10 +14,10 @@ int main(void)
 	char s2[] = "HelloWW";
 
 	printf("%d\n", strcmp(s1, s2));
-        printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", strcmp(s2, s1));
-        printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", strcmp(s1, s1));
-        printf("%d\n", _strcmp(s1, s1));
+	printf("%d\n", _strcmp(s1, s1));
 	return (0);
 }
