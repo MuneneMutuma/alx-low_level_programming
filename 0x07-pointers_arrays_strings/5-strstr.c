86b93afc58f1b24a7902a6e501ec9fdc79488fbd
Munene <muneneemasonmutuma@gmail.com>
2022-03-28 22:55:42 +0300

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+/**
+ * _strstr - locates a substring
+ *
+ * @haystack: pointer to bigger string
+ * @needle: pointer to substring
+ *
+ * Return: pointer to beginning of substring if found
+ *	   NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (needle[0] == haystack[i])
+		{
+			for (j = 0; needle[j] == haystack[i + j]; j++)
+				;
+
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+
+	return ('\0');
+}
