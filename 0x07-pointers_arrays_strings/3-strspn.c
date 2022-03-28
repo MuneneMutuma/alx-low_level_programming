2e4cbeb2a67c930bcc067c38ac2e7bce4140ca89
Munene <muneneemasonmutuma@gmail.com>
2022-03-28 21:49:36 +0300

    3.3 corrected inverted indices and arrays

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,18 +10,18 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; accept[i]; i++)
+	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; s[j]; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 				break;
 		}
 
 		if (!accept[j])
-			return (i + 1);
+			return (i);
 
 	}
 
-	return (i + 1);
+	return (i);
 }
