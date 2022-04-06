2e5fdb63e6cd5e45feefa571a46bf24437deab2c
Munene <muneneemasonmutuma@gmail.com>
2022-04-06 12:28:29 +0300

    1. string duplicator

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+int _strlen(char *str);
+
+/**
+ * _strdup - copies a string into a dynamically allocated string
+ *
+ * @str: pointer to string to be copied
+ *
+ * Return: `NULL` if str=NULL
+ *	   pointer to duplicated string
+ *	   `NULL` if insucfficient memory available
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *dup = malloc(sizeof(char) * _strlen(str));
+
+	if (!str)
+		return (0);
+
+	if (!dup)
+		return (0);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		dup[i] = str[i];
+		i++;
+	}
+
+	return (dup);
+}
+
+/**
+ * _strlen - finds the length of a string
+ *
+ * @str: pointer of the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
