e2dc9f9c061d12ccada96fe31646055d345c46bd
Munene <muneneemasonmutuma@gmail.com>
2022-04-07 22:43:38 +0300

    2.1 correction trial 1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,11 +11,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *str = malloc(size * nmemb);
+	char *str;
 	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
+
+	str = malloc(nmemb * size);
 
 	if (!str)
 		return (0);
