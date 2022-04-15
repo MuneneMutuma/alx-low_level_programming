2e8078e46308e07b138f5560cb043afaff8091e8
Munene <muneneemasonmutuma@gmail.com>
2022-04-15 10:33:24 +0300

    0. print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+/**
+ * print_name - a function that prints a name
+ *
+ * @name: the name to be printed
+ * @f: printer function
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*func)(char *);
+
+	func = f;
+
+	func(name);
+}
