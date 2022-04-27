4f69bae725a51b1a2d911bff0ef035bd6b48f782
Munene <muneneemasonmutuma@gmail.com>
2022-04-27 12:19:02 +0300

    0. poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+#Structures(`struct`) and `typedef` in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure for dog name age and owner
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
