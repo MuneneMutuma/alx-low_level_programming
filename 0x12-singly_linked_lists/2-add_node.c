adc9ca36bb6860442c20b7f2d2f1aaaa322e90a2
Munene <muneneemasonmutuma@gmail.com>
2022-05-01 15:51:03 +0300

    2.i trial handle for malloc return correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,10 +17,15 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-	new_node->str = strdup(str);
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
+	if (new_node)
+	{
+		new_node->str = strdup(str);
+		new_node->len = strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+		free(new_node);
+	}
+
 	if (!*head)
 		return (NULL);
 	return (*head);
