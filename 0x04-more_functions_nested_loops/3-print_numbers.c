9243e4c6604b114952ce35a3bbbac9b6cc59a4b1
Munene <muneneemasonmutuma@gmail.com>
2022-06-12 12:36:32 +0300

    3. print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Returns: void
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
