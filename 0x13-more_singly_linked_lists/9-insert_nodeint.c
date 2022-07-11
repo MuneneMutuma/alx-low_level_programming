67c64da18305d2700d9a8e0e95df64744df3086e
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-11 11:43:56 +0300

    9.1 add node at the beginning of list, and also if head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,15 +14,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *iter, *node;
 
-	if (!*head)
-		return (NULL);
-
-	iter = *head;
 	node = malloc(sizeof(listint_t));
 	if (!node)
 		return (NULL);
 
 	node->n = n;
+	if (!*head)
+	{
+		node->next = NULL;
+		*head = node;
+		return (node);
+	}
+
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+	}
+
+	iter = *head;
 	for (i = 0; i < idx - 1 && iter; i++)
 		iter = iter->next;
 
