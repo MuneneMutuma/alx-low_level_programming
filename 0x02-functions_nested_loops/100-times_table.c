c70627f6a48e0fa3be7d71a50cd782a56af30e2c
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-07-13 12:33:06 +0300

    100.1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,6 +10,11 @@ void print_times_table(int n)
 {
 	int i, j;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	if (n < 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
@@ -17,43 +22,58 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				int product = i * j;
-				if (j == 0)
-				{
-					_putchar(j + '0');
-				}
 
-				if ((product / 100) > 0)
-				{
-					int hundreds = product / 100;
-					int tens = ((product % 100) - (product % 10)) / 10;
-					int ones = (product % 10);
-					_putchar(hundreds + '0');
-					_putchar(tens + '0');
-					_putchar(ones + '0');
-				}
-
-				else if ((product / 10) > 0 && (product / 100) == 0)
-				{
-					int tens = product / 10;
-					int ones = product % 10;
-					_putchar(' ');
-					_putchar(tens + '0');
-					_putchar(ones + '0');
-				}
-				else if ((product >= 0) && j != 0 && product < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product + '0');
-				}
-				if (j != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				printer(product, j, n);
 			}
 			_putchar('\n');
 		}
-		
+	}
+}
+
+
+/**
+ * printer - utility function for printing
+ *
+ * @product: product to be printed
+ * @j: inner limit
+ * @n: number
+ *
+ * Return: void
+ */
+void printer(int product, int j, int n)
+{
+	if (j == 0)
+		_putchar(j + '0');
+
+	if ((product / 100) > 0)
+	{
+		int hundreds = product / 100;
+		int tens = ((product % 100) - (product % 10)) / 10;
+		int ones = (product % 10);
+
+		_putchar(hundreds + '0');
+		_putchar(tens + '0');
+		_putchar(ones + '0');
+	}
+
+	else if ((product / 10) > 0 && (product / 100) == 0)
+	{
+		int tens = product / 10;
+		int ones = product % 10;
+
+		_putchar(' ');
+		_putchar(tens + '0');
+		_putchar(ones + '0');
+	}
+	else if ((product >= 0) && j != 0 && product < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(product + '0');
+	}
+	if (j != n)
+	{
+		_putchar(',');
+		_putchar(' ');
 	}
 }
