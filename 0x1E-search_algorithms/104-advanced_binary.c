6be7844825410e157d9411df275874d8001c9df3
Munene Mutuma <muneneemasonmutuma@gmail.com>
2022-10-05 16:29:50 +0300

    104.2 advanced binary + recursive

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include "search_algos.h"
 
+
+int recursive_binary(int *array, size_t start, size_t end, int value);
+
 /**
  * advanced_binary - performs a binary search
  *
@@ -12,32 +15,47 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int first, last, index;
+	return (recursive_binary(array, 0, size - 1, value));
+}
 
-	first = 0;
-	last = (int)size - 1;
-	index = (int)(first + last) / 2;
+/**
+ * recursive_binary - performs binary search recursively
+ *
+ * @array: array to be searched
+ * @start: start index for slicing array
+ * @end: end index for slicing array
+ * @value: value to be searched for against
+ *
+ * Return: index if value found, -1 otherwise
+ */
+int recursive_binary(int *array, size_t start, size_t end, int value)
+{
+	size_t mid;
+
+	mid = (start + end) / 2;
 
 	if (array == NULL)
 		return (-1);
-	for (; last >= first; index = (first + last) / 2)
+
+	if (start == end && array[mid] != value)
+		return (-1);
+
+	if (array[mid] == value)
 	{
-		printf("Searching in array: ");
-		print_array(array, first, last);
-		printf("\n");
-		if (value == array[index])
-		{
-			if (value == array[index - 1])
-			{
-				size = (size_t)(index - first);
-				index = advanced_binary(array, size, value);
-			}
-			return (index);
-		}
-		else if (value < array[index])
-			last = index - 1;
-		else
-			first = index + 1;
+		print_array(array, start, end);
+		if (array[mid - 1] == value)
+			return (recursive_binary(array, start, mid, value));
+		return (mid);
+	}
+	else if (array[mid] < value)
+	{
+		print_array(array, start, end);
+		return (recursive_binary(array, mid + 1, end, value));
+	}
+	else if (array[mid] > value)
+	{
+		print_array(array, start, end);
+		return (recursive_binary(array, start, mid - 1, value));
 	}
 
 	return (-1);
@@ -56,10 +74,15 @@ void print_array(int *array, int start, int end)
 {
 	int i;
 
+	if (array == NULL)
+		return;
+
+	printf("Searching in array: ");
 	for (i = start; i < end + 1; i++)
 	{
 		printf("%d", array[i]);
 		if (i != end)
 			printf(", ");
 	}
+	printf("\n");
 }
